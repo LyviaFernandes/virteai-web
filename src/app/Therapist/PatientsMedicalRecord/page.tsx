d1039a69ca4704ad5dc53a4e5c229c7a7969ff92
LyviaFernandes <lyviafernandesborges@gmail.com>
2026-04-23 13:33:00 -0300

    create card evolution

--- src/app/Therapist/PatientsMedicalRecord/page.tsx
+++ src/app/Therapist/PatientsMedicalRecord/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import { useRef, useState, useEffect } from 'react';import './style.css'
 import './style.css'
 import Image from 'next/image';
 import HeaderEnter from '@/components/header-enter/HeaderEnter';
@@ -11,6 +11,8 @@ import progess from '@/assets/images/ProgressDiagram.svg';
 import visualize from '@/assets/images/visualizeicon.svg';
 import edit from '@/assets/images/editicon.svg';
 import Footer from '@/components/footer/Footer';
+import Input from '@/components/input/Input';
+
 
 type User = {
     id: number;
@@ -20,12 +22,21 @@ type User = {
 }
 
 export default function PatientsMedicalRecord () {
+    const [date, setDate] = useState('');
+    const [goal, setGoal] = useState('');
+    const [pontuation, setPontuation] = useState('');
 
     const user: User = {
         id: 1,
         name: "João Lucas Vega",
         profileImage: "https://thumbs.dreamstime.com/b/retrato-da-pessoa-adulta-22170035.jpg",
         status: "Em acompanhamento"
+    };
+    const [showCodeInput, setShowCodeInput] = useState(false);
+    const codeBoxRef = useRef<HTMLDivElement>(null);
+
+    const handleAccessScenario = () => {
+    setShowCodeInput(true);
     };
 
     return(
@@ -62,6 +73,7 @@ export default function PatientsMedicalRecord () {
             <div className="DiagramTitle">
                 <p>Evolução geral</p>
                 <Image 
+                    onClick={handleAccessScenario}
                     src={plusicon}
                     alt=""
                     className="plus-image"
@@ -74,6 +86,58 @@ export default function PatientsMedicalRecord () {
                     className="progess-image"
                     />
             </div>
+
+            {showCodeInput && (
+                <div className="overlay" onClick={() => setShowCodeInput(false)}>
+                    <div
+                        className="code-box"
+                        ref={codeBoxRef}
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <div className="evolution">
+                            <h2>Evolução Geral</h2>
+                        </div>
+
+                        <div className="text-evolution">
+                            <p>Preencha as informações abaixo, para adicionar ao 
+                            gráfico de evolução de seu paciente:</p>
+                        </div>
+                        
+                        <div className="inputs-section">
+                            <div className="input-box">
+                                <p>Data da consulta:</p>
+                                <Input
+                                    description='Insira a data da consulta realizada'
+                                    value={date}
+                                    onChange={(e) => setDate (e.target.value)}
+                                />
+                            </div>
+
+                            <div className="input-box">
+                                <p>Objetivo da consulta:</p>
+                                <Input
+                                    description='Insira o objetivo da consulta realizada'
+                                    value={goal}
+                                    onChange={(e) => setGoal (e.target.value)}
+                                />
+                            </div>
+
+                            <div className="input-box">
+                                <p>Pontuação</p>
+                                <Input
+                                    description='Avalie a evolução do paciente de 0-100  '
+                                    value={pontuation}
+                                    onChange={(e) => setPontuation (e.target.value)}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="button-send">
+                            <button>Enviar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
 
 
             <div className="therapeutic-goals">
@@ -101,7 +165,7 @@ export default function PatientsMedicalRecord () {
 
                 <div className="therapeutic-goals-container">
                     <div className="therapeutic-goals__content">
-                        <h2>Objetivos terapeuticos</h2>
+                        <h2>Relatórios</h2>
                     </div>
 
                     <Image 
